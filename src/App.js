706bd9f62408e238ef0ad55e78ecf850de8f97cb
mark-williams <markwill@hotmail.co.uk>
2018-06-27 09:57:10 +0100

    chore: remove css file and use styled-components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Beers from './components/Beers';
+import styled from 'styled-components';
+
+const AppStyle = styled.div`
+  text-align: center;
+
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 80px;
+  }
+
+  .App-header {
+    background-color: #222;
+    height: 150px;
+    padding: 20px;
+    color: white;
+  }
+
+  .App-title {
+    font-size: 1.5em;
+  }
+
+  .App-intro {
+    font-size: large;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppStyle>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome</h1>
         </header>
         <Beers />
-      </div>
+      </AppStyle>
     );
   }
 }
