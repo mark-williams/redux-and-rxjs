de45bb45d214ce53a2246c56a3e4c4d1c7cd51f0
mark-williams <markwill@hotmail.co.uk>
2018-06-28 16:01:43 +0100

    chore: enable redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,10 @@ const initialState = {
   beers: []
 };
 
-const store = createStore(beersReducer, initialState);
+const store = createStore(
+  beersReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
