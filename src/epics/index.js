2ba48ec7a1f7b90d7f39dfcdb8dc668f29d05c35
mark-williams <markwill@hotmail.co.uk>
2018-06-29 08:55:44 +0100

    feat: debounce serach input to reduce the number of api calls

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,6 +1,6 @@
 import { combineEpics } from 'redux-observable';
 import { ajax } from 'rxjs/ajax';
-import { tap, switchMap, map } from 'rxjs/operators';
+import { tap, switchMap, map, debounceTime } from 'rxjs/operators';
 import { actionTypes } from '../actions';
 
 const searchApiRoot = 'https://api.punkapi.com/v2/beers?beer_name=';
@@ -9,6 +9,7 @@ const ajaxCall = searchTerm => ajax.getJSON(getSearchUrl(searchTerm));
 
 const loadBeersEpic = action$ => {
   return action$.ofType(actionTypes.SEARCH_INPUT).pipe(
+    debounceTime(1000),
     // eslint-disable-next-line
     tap(x => console.log('ACTION', x)),
     switchMap(action => {
